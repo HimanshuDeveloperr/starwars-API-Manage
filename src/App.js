ac88578bdf10aa3b9d5319cad39a5a935451cb25
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-02-20 13:20:54 +0530

    ErrorHandling usedState IMP...

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading,setIsLoading]=useState(false);
+  const [errorFound,setErrorFound]=useState(null)
    
 
   async function fetchingMovies() {
     setIsLoading(true)
+    setErrorFound(null)
   try {
-    const response = await fetch("https://swapi.dev/api/films/");
+    const response = await fetch("https://swapi.dev/api/film/");
+    if(!response.ok){
+      throw new Error("something went wrong")
+    }
     const data = await response.json();
     console.log(response.status)
+
+
+
     const transformDataNames = data.results.map((arrofmovies) => {
       return {
         id: arrofmovies.episode_id,
@@ -23,10 +31,14 @@ function App() {
       };
     });
     setMovies(transformDataNames);
-    setIsLoading(false)
+    
   } catch (error) {
-    console.error(error);
+    setErrorFound(error.message);
+        
+
   }
+      setIsLoading(false)
+
 }
 
     
@@ -40,6 +52,7 @@ function App() {
       <section>
         {!isLoading&& movies.length>0 &&<MoviesList movies={movies} />}
         {!isLoading && movies.length===0 && <p>Found no movies.</p>}
+        {!isLoading && errorFound && <p>{errorFound}</p>}
         {isLoading&&<p>Loading...</p>}
       </section>
     </React.Fragment>
