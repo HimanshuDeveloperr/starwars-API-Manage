e906f38f6c1aeff2b9a8dc51e503e0c6398e881e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-02-24 20:04:14 +0530

    fetch movies UseCallback & UseEffect

--- src/App.js
+++ src/App.js
@@ -1,82 +1,76 @@
-import React, { useCallback, useState } from "react";
-import { Button } from "react-bootstrap"
+import React, { useCallback, useState,useEffect } from "react";
+import { Button } from "react-bootstrap";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [errorFound, setErrorFound] = useState(null)
-  useState(0)
-
-
+  const [errorFound, setErrorFound] = useState(null);
+  
+  
+  
   const fetchingMovies = useCallback(async () => {
-    setIsLoading(true)
-    setErrorFound(null)
+    setIsLoading(true);
+    setErrorFound(null);
     try {
-      const response = await fetch("https://swapi.dev/api/film/");
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong ....Retrying')
-
+        throw new Error("Something went wrong ....Retrying");
       }
       const data = await response.json();
-      console.log(response.status)
-
-
-
+      console.log(response.status);
+      
       const transformDataNames = data.results.map((arrofmovies) => {
         return {
           id: arrofmovies.episode_id,
           title: arrofmovies.title,
           openingText: arrofmovies.opening_crawl,
-          releaseDate: arrofmovies.release_date
+          releaseDate: arrofmovies.release_date,
         };
       });
       setMovies(transformDataNames);
-
     } catch (error) {
       setErrorFound(error.message);
-
-
-
     }
-    setIsLoading(false)
+    setIsLoading(false);
+  }, []);
+  
+  useEffect(() => {
+    fetchingMovies ()
+  }, [fetchingMovies])
 
-  }, [])
-
+  
   if (errorFound) {
-
     var id = setTimeout(fetchingMovies, 5000);
   }
 
   let cancelHandler = useCallback(() => {
-    clearTimeout(id)
-    setErrorFound(null)
+    clearTimeout(id);
+    setErrorFound(null);
+  }, [id]);
 
-
-  }, [id])
-
-
-
-
-  let content = <p>Found no movies.</p>
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0 && !isLoading) {
-
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} />;
   }
 
   if (errorFound) {
-
-    content = <p>{errorFound}
-      <div style={{ margin: '2px' }}>
-        <Button style={{ color: "white" }} onClick={cancelHandler}>Cancel</Button>
-      </div>
-    </p>
+    content = (
+      <p>
+        {errorFound}
+        <div style={{ margin: "2px" }}>
+          <Button style={{ color: "white" }} onClick={cancelHandler}>
+            Cancel
+          </Button>
+        </div>
+      </p>
+    );
   }
 
   if (isLoading) {
-    content = <p>Loading...</p>
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -84,9 +78,7 @@ function App() {
       <section>
         <button onClick={fetchingMovies}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
